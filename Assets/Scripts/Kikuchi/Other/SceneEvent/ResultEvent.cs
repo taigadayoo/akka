8284b2a67b272d523c62e77bf69fa5b4ccefa43d
sakanachqn <sakanachqn@gmail.com>
2025-01-20 11:07:43 +0900

    Fix Result Event Bug

--- Assets/Scripts/Kikuchi/Other/SceneEvent/ResultEvent.cs
+++ Assets/Scripts/Kikuchi/Other/SceneEvent/ResultEvent.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -9,83 +9,100 @@ using Unity.VisualScripting;
 
 public class ResultEvent : MonoBehaviour
 {
-    private bool _isClear = false;
-    private float _clearTime = 0.0f;
-    private int _missCount = 0;
-
-
-    [SerializeField]
-    private List<float> _clearTimeLimit = new List<float>();
+    private bool _isClear = false;          // ゲームクリアフラグ
+    private float _clearTime = 0.0f;       // クリアタイム
+    private int _missCount = 0;            // ミス回数
 
     [SerializeField]
-    private List<ResultSheetData> _resultSpriteCollection = new List<ResultSheetData>();
-    private ResultSheetData _currentResultSheetData = null;
+    private List<float> _clearTimeLimit = new List<float>();  // クリアタイム制限のリスト
 
-    private Tween _tween = null;
+    [SerializeField]
+    private List<ResultSheetData> _resultSpriteCollection = new List<ResultSheetData>();  // リザルトシートデータのリスト
+    private ResultSheetData _currentResultSheetData = null;  // 現在のリザルトシートデータ
 
-    // Start is called before the first frame update
+    private Tween _tween = null;  // DOTweenのアニメーショントゥイーン
+
+
     void Start()
     {
-        SetText();
-        DisablePlayerPanel();
-        PanelTween();
-        AudioManager.Instance.PlayBGM("ポケットワールド");
-        ResultEventHandler();
+        // スコアを取得する
+        GetScore();
 
+        // ランクを取得する
+        GetRank();
+
+        // プレイヤーパネルを非表示にする
+        DisablePlayerPanel();
+
+        // テキストを設定する
+        SetText();
+
+        // パネルをアニメーションさせる
+        PanelTween();
+
+        // リザルトイベントハンドラを設定する
+        ResultEventHandler();
     }
 
+    // リザルトイベントハンドラ
     private void ResultEventHandler()
     {
+        // コントローラーの入力イベントを購読する
         ControllerManager.Instance.OnControllerData
-        .Where(_ => !SceneManager.Instance.IsFading)
-        .Where(_ => _tween == null)
-        .Subscribe(controllerData =>
-        {
-            Debug.Log($"Player: {controllerData.PlayerType}, Input: {controllerData.ActionType}");
-
-            switch (controllerData.ActionType)
+            .Where(_ => !SceneManager.Instance.IsFading)  // シーン遷移中でないことを確認
+            .Where(_ => _tween == null)  // アニメーション中でないことを確認
+            .Subscribe(controllerData =>
             {
-                case ActionType.Buttons:
-                    HandleButtonInput(controllerData);
-                    if(CheckPlayerPanelActive())
-                    {
-                        SceneManager.Instance.LoadScene(SceneName.Title);
-                        AudioManager.Instance.StopBGM();
-                    }
-                    break;
-                default:
-                    Debug.LogError("Invalid Action Type");
-                    break;
-            }
+                Debug.Log($"Player: {controllerData.PlayerType}, Input: {controllerData.ActionType}");
 
-        })
-        .AddTo(this);
+                // 入力タイプに応じて処理を分岐
+                switch (controllerData.ActionType)
+                {
+                    case ActionType.Buttons:
+                        // ボタン入力の場合
+                        HandleButtonInput(controllerData);
+
+                        // 全プレイヤーのパネルが表示されていたらタイトルシーンに遷移する
+                        if (CheckPlayerPanelActive())
+                        {
+                            SceneManager.Instance.LoadScene(SceneName.Title);
+                        }
+                        break;
+                    default:
+                        Debug.LogError("Invalid Action Type");
+                        break;
+                }
+            })
+            .AddTo(this);
     }
 
+    // ボタン入力処理
     private void HandleButtonInput(ControllerData controllerData)
     {
+        // Eastボタンが押されたら対応するプレイヤーのパネルを表示する
         if (controllerData.ButtonType == ButtonType.East)
         {
             ShowPlayerPanel(controllerData.PlayerType);
         }
     }
 
+    // 全プレイヤーのパネルが表示されているかチェックする
     private bool CheckPlayerPanelActive()
     {
         return _currentResultSheetData.PL1OkImage.activeSelf && _currentResultSheetData.PL2OkImage.activeSelf;
     }
 
+    // プレイヤーのパネルを表示する
     private void ShowPlayerPanel(PlayerType playerType)
     {
+        // プレイヤータイプに応じて対応するパネルを表示する
         switch (playerType)
         {
             case PlayerType.Player1:
                 _currentResultSheetData.PL1OkImage.SetActive(true);
-                AudioManager.Instance.PlaySE("はんこ", 0.8f);
                 break;
             case PlayerType.Player2:
                 _currentResultSheetData.PL2OkImage.SetActive(true);
-                AudioManager.Instance.PlaySE("はんこ", 0.8f);
                 break;
             default:
                 Debug.LogWarning($"Unsupported PlayerType: {playerType}");
@@ -93,50 +110,55 @@ public class ResultEvent : MonoBehaviour
         }
     }
 
+    // プレイヤーパネルを非表示にする
     private void DisablePlayerPanel()
     {
         _currentResultSheetData.PL1OkImage.SetActive(false);
         _currentResultSheetData.PL2OkImage.SetActive(false);
     }
 
-
-
+    // スコアを取得する
     private void GetScore()
     {
-        _isClear = PlayerPrefs.GetInt("IsClear") == 0;
+        _isClear = PlayerPrefs.GetInt("IsClear") == 0; // 0: クリア, 1: 失敗
         _clearTime = PlayerPrefs.GetFloat("ClearTime");
         _missCount = PlayerPrefs.GetInt("MissCount");
     }
 
+    // ランクを取得する
     private void GetRank()
     {
-        if(_isClear)
+        // クリアしている場合
+        if (_isClear)
         {
-            for(int i = 0; i < _clearTimeLimit.Count; i++)
+            // クリアタイム制限リストをループして、クリアタイムが制限時間内であれば対応するリザルトシートデータを設定する
+            for (int i = 0; i < _clearTimeLimit.Count; i++)
             {
-                if(_clearTime < _clearTimeLimit[i])
+                if (_clearTime < _clearTimeLimit[i])
                 {
                     _currentResultSheetData = _resultSpriteCollection[i];
                     break;
                 }
             }
         }
+        // 失敗している場合
         else
         {
+            // 失敗用のリザルトシートデータを設定する
             _currentResultSheetData = _resultSpriteCollection[_resultSpriteCollection.Count - 1];
         }
     }
 
+    // テキストを設定する
     private void SetText()
     {
-        GetRank();
-        GetScore();
-
+        // クリアテキスト、タイムテキスト、ミステキストを設定する
         _currentResultSheetData.ClearText.text = _isClear ? "成功！" : "失敗...";
-        _currentResultSheetData.TimeText.text =　ConvertToTime(_clearTime);
+        _currentResultSheetData.TimeText.text = ConvertToTime(_clearTime);
         _currentResultSheetData.MissText.text = _missCount.ToString() + "回";
     }
 
+    // 時間を文字列に変換する
     private string ConvertToTime(float time)
     {
         int minute = (int)time / 60;
@@ -144,12 +166,17 @@ public class ResultEvent : MonoBehaviour
         return minute.ToString("00") + ":" + second.ToString("00") + "秒";
     }
 
+    // パネルをアニメーションさせる
     private void PanelTween()
     {
-        AudioManager.Instance.PlaySE("チラシ01", 0.8f);
+        // リザルトパネルを下から上に移動させるアニメーション
         RectTransform rect = _currentResultSheetData.gameObject.GetComponent<RectTransform>();
-        _tween = rect.DOMoveY(0, 1.0f).SetEase(Ease.OutBounce).OnComplete(() => _tween = null);
+        _tween = rect.DOMoveY(0, 1.0f)
+            .SetEase(Ease.OutBounce)
+            .OnComplete(() =>
+            {
+                _tween = null;  // アニメーション完了後にトゥイーンをnullにする
+                SetText();  // アニメーション後にテキストを更新
+            });
     }
-
-
 }
